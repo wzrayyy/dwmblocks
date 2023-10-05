edb220011facb25386f94a6d1b5ca85fe373fa8b
Arthur <me@wzray.ru>
2023-10-06 02:38:34 +0300

    move blocks around

--- blocks.h
+++ blocks.h
@@ -1,14 +1,8 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/							/*Update Interval*/	/*Update Signal*/
-	{"Mem:", 	"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,			0},
-	{"", 		"date '+%b %d (%a) %I:%M%p'",					5,			0},
-	{"", 		"$HOME/.local/share/scripts/sb-battery",			5,			0},
-
+  // <i> <cmd> <int> <sig>
+	{"", "sb-battery",			          5, 0},
+	{"", "date +'%a %d/%m %I:%M %P'", 5, 0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
-
-// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; make && { killall -q dwmblocks;setsid dwmblocks & }
