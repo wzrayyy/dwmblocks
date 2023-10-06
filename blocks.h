66f712c6698f7d4e32adb8690a7a8ec703299fac
Arthur <me@wzray.ru>
2023-10-06 10:20:30 +0300

    fix date format

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 static const Block blocks[] = {
-  // <i> <cmd> <int> <sig>
-	{"", "sb-battery",			          5, 0},
-	{"", "date +'%a %d/%m %I:%M %P'", 5, 0},
+	// <i> <cmd> 			    <int> <sig>
+	{"",   "sb-battery",		    5,    0},
+	{"",   "date +'%a %m/%d %I:%M %P'", 5,    0},
 };
 
 static char delim[] = " | ";
