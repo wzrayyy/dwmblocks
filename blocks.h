e7e5ed3f64ad16cd972a11001a6965b3b75689d6
Arthur Khachaturov <me@wzray.ru>
2024-08-31 13:49:26 +0300

    add statusbar elements

--- blocks.h
+++ blocks.h
@@ -1,7 +1,9 @@
 static const Block blocks[] = {
-	// <i> <cmd> 			    <int> <sig>
-	{"",   "sb-battery",		    5,    0},
-	{"",   "date +'%a %m/%d %I:%M %P'", 5,    0},
+//  <i>	   <cmd>                         <int> <sig>
+	{"",   "sb-vpn",					 30,   2},
+	{"",   "sb-cast",					 30,   1},
+	{"",   "sb-battery",				 5,    0},
+	{"",   "date +'%a %m/%d %I:%M %P'",  5,    0},
 };
 
 static char delim[] = " | ";
